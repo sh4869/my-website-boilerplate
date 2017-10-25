1ae0b0d1fc86252096516cde489cb722614ca155
sh4869 <nobuk4869@gmail.com>
2017-10-25 11:20:28 +0900

    Update boilerplate
    * add gulp-connect for webserver and livereload
    * fix name of directory
    * add build task for default

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const plumber = require("gulp-plumber");
 const pug = require("gulp-pug");
 const data = require("gulp-data");
 const sass = require("gulp-sass");
+const connect = require('gulp-connect');
 const runSequence = require("run-sequence");
 const moduleImporter = require("sass-module-importer");
 
-
 gulp.task('pug', () => {
-    gulp.src(["www/pug/**/*.pug","!www/pug/include/*.pug"], { base: "www/pug/" })
+    gulp.src(["www/pug/**/*.pug", "!www/pug/include/*.pug"], { base: "www/pug/" })
         .pipe(plumber({
             errorHandler: (err) => {
                 console.log(err);
@@ -17,7 +17,7 @@ gulp.task('pug', () => {
         }))
         .pipe(data(
             (file) => {
-                const dirname = __dirname + "/www/datas/";
+                const dirname = __dirname + "/www/data/";
                 const files = fs.readdirSync(dirname);
                 let datas = {};
                 files.forEach((name) => {
@@ -27,7 +27,8 @@ gulp.task('pug', () => {
             })
         )
         .pipe(pug({ pretty: true }))
-        .pipe(gulp.dest("dest/"));
+        .pipe(gulp.dest("dest/"))
+        .pipe(connect.reload());
 })
 
 gulp.task("img", () => {
@@ -43,32 +44,41 @@ gulp.task("css", () => {
             }
         }))
         .pipe(sass({ outputStyle: 'expanded', importer: moduleImporter() }).on('error', sass.logError))
-        .pipe(gulp.dest("dest/css/"));
+        .pipe(gulp.dest("dest/css/"))
+        .pipe(connect.reload());
 });
 
-gulp.task("font",() => {
+gulp.task("font", () => {
     return gulp.src("node_modules/font-awesome/fonts/*")
         .pipe(gulp.dest("dest/fonts/"));
 });
 
-gulp.task("favicon",() => {
+gulp.task("favicon", () => {
     return gulp.src("www/favicon/*")
         .pipe(gulp.dest("dest/favicon/"));
 })
 
-gulp.task("cname",() => {
+gulp.task("cname", () => {
     return gulp.src("www/CNAME")
         .pipe(gulp.dest("dest/"));
 })
 
-gulp.task("build", ["img", "pug", "css","font","cname","favicon"], () => {
+gulp.task("build", ["img", "pug", "css", "font", "cname", "favicon"], () => {
 
 });
 
 gulp.task('watch', () => {
-    gulp.watch(["www/pug/**/*.pug","www/datas/**/*.json"], ["pug"]);
+    gulp.watch(["www/pug/**/*.pug", "www/datas/**/*.json"], ["pug"]);
     gulp.watch(["www/scss/**/*.scss"], ["css"]);
     gulp.watch(["www/img/**/*.png", "www/img/**/*.jpg"], ["img"])
 })
 
-gulp.task('default', ["watch"]);
+gulp.task('connect', () => {
+    connect.server({
+        root: "dest",
+        livereload: true,
+        port: 9000
+    })
+});
+
+gulp.task('default', ["build", "watch","connect"]);
